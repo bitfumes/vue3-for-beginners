9f9a25339ebf6970991ed61b765860ba29d07c68
sarthaksavvy <bitfumes@gmail.com>
2020-09-30 21:14:56 +0530

    8. Vue3 - v-if v-else v-else-if

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <h1 v-html="title"></h1>
+  <h1 v-show="isShowing">{{ count }}</h1>
+  <!-- <h3 v-else-if="isShowing === null">This is v-else-if</h3>
+  <h2 v-else>This is v-else part</h2> -->
   <p>SOmething else</p>
 </template>
 
@@ -8,7 +10,14 @@ export default {
   data() {
     return {
       title: "<h1>This is title inside h1 tag</h1>",
+      isShowing: false,
+      count: 0,
     };
+  },
+  mounted() {
+    setInterval(() => {
+      this.count++;
+    }, 1000);
   },
 };
 </script>
