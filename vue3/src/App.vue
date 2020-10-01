df22afafc15948b964ab480d488a64a0bdfc3b9a
sarthaksavvy <bitfumes@gmail.com>
2020-10-01 21:45:22 +0530

    14. Vue3 - methods

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -2,20 +2,28 @@
   <h1>Dc Heros</h1>
   <ul>
     <li v-for="(hero, index) in dcHeros" :key="index">
-      {{ index }} : {{ hero.name }}
+      {{ hero.name }}
     </li>
   </ul>
-  <form @submit.prevent="newHero = 'Wonder Woman'">
-    <input v-model="newHero" />
+  <form @submit.prevent="addHero">
+    <input v-model="newHero" placeholder="Type Hero Name Here" />
     <button type="submit">Add Hero</button>
   </form>
 </template>
 
 <script>
 export default {
+  methods: {
+    addHero() {
+      if (this.newHero !== "") {
+        this.dcHeros.unshift({ name: this.newHero });
+        this.newHero = "";
+      }
+    },
+  },
   data() {
     return {
-      newHero: "Aquaman",
+      newHero: "",
       dcHeros: [
         { name: "SuperGirl" },
         { name: "Flash" },
