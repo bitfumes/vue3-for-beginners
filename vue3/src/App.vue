555ebaefd56048b6c20cd6220c4f811d00d5b92e
sarthaksavvy <bitfumes@gmail.com>
2020-10-02 11:54:35 +0530

    16. Vue3 - why unique key

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -1,24 +1,15 @@
 <template>
   <h1>Dc Heros {{ herosCount }}</h1>
-  <h2>{{ fullname }}</h2>
-  <h2>{{ fname }} {{ lname }}</h2>
-  <p>{{ randC }}</p>
-  <p>{{ randC }}</p>
-  <p>{{ randC }}</p>
-  <p>{{ randM() }}</p>
-  <p>{{ randM() }}</p>
-  <p>{{ randM() }}</p>
   <ul>
-    <li v-for="(hero, index) in dcHeros" :key="index">
-      {{ hero.name }}
+    <li v-for="(hero, index) in dcHeros" :key="hero.name">
+      <div>{{ hero.name }} <button v-on:click="remove(index)">x</button></div>
+      <input />
     </li>
   </ul>
   <form @submit.prevent="addHero">
     <input v-model="newHero" placeholder="Type Hero Name Here" />
     <button type="submit">Add Hero</button>
   </form>
-  <button @click="setFullName">Set FUll Name</button>
-  <small>Total Heros in the list is {{ herosCount }}</small>
 </template>
 
 <script>
@@ -26,19 +17,6 @@ export default {
   computed: {
     herosCount() {
       return this.dcHeros.length;
-    },
-    randC() {
-      return this.dcHeros.length + Math.random();
-    },
-    fullname: {
-      get() {
-        return `Full name is ${this.fname} ${this.lname}`;
-      },
-      set(fullname) {
-        const values = fullname.split(" ");
-        this.fname = values[0];
-        this.lname = values[1];
-      },
     },
   },
   methods: {
@@ -48,18 +26,13 @@ export default {
         this.newHero = "";
       }
     },
-    randM() {
-      return Math.random();
-    },
-    setFullName() {
-      this.fullname = "Bitfumes Tutorial";
+    remove(index) {
+      this.dcHeros = this.dcHeros.filter((hero, i) => i != index);
     },
   },
   data() {
     return {
       newHero: "",
-      fname: "Sarthak",
-      lname: "Shrivastava",
       dcHeros: [
         { name: "SuperGirl" },
         { name: "Flash" },
