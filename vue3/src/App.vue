ed09bb5c84e60f139f368f13e39c806b3394c683
sarthaksavvy <bitfumes@gmail.com>
2020-10-02 12:38:04 +0530

    18. Vue3 - components

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -1,66 +1,15 @@
 <template>
+  <AppHeader />
   <div class="w-full flex">
-    <div class="m-auto">
-      <h1 class="text-2xl text-center">Dc Heros {{ herosCount }}</h1>
-      <ul>
-        <li
-          class="flex justify-between"
-          v-for="(hero, index) in dcHeros"
-          :key="hero.name"
-        >
-          <div>
-            {{ hero.name }}
-          </div>
-          <button v-on:click="remove(index)">x</button>
-        </li>
-      </ul>
-      <form class="mt-10" @submit.prevent="addHero">
-        <input
-          class="border rounded"
-          v-model="newHero"
-          placeholder="Type Hero Name Here"
-        />
-        <button
-          class="border rounded bg-gradient-to-r from-red-700 to-pink-500 text-white"
-          type="submit"
-        >
-          Add Hero
-        </button>
-      </form>
-    </div>
+    <DcHeros />
   </div>
 </template>
 
 <script>
+import AppHeader from "./components/AppHeader";
+import DcHeros from "./components/DcHeros";
 export default {
-  computed: {
-    herosCount() {
-      return this.dcHeros.length;
-    },
-  },
-  methods: {
-    addHero() {
-      if (this.newHero !== "") {
-        this.dcHeros.unshift({ name: this.newHero });
-        this.newHero = "";
-      }
-    },
-    remove(index) {
-      this.dcHeros = this.dcHeros.filter((hero, i) => i != index);
-    },
-  },
-  data() {
-    return {
-      newHero: "",
-      dcHeros: [
-        { name: "SuperGirl" },
-        { name: "Flash" },
-        { name: "Batman" },
-        { name: "Arrow" },
-        { name: "SuperMan" },
-      ],
-    };
-  },
+  components: { AppHeader, DcHeros },
 };
 </script>
 
---
+++ vue3/src/components/AppHeader.vue
@@ -0,0 +1,14 @@
+<template>
+  <nav
+    class="w-full bg-gradient-to-r from-blue-800 to-blue-600 text-white px-4 py-2"
+  >
+    <a>Dc Heros</a>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
