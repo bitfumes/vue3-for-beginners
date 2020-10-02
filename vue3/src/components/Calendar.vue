87b6ad46e0a99d09e72d94df2784b813b0cbf9fa
sarthaksavvy <bitfumes@gmail.com>
2020-10-02 20:57:17 +0530

    20. Vue3 - Calendar app part 2

--- vue3/src/components/Calendar.vue
+++ vue3/src/components/Calendar.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="m-auto">
     <h1 class="text-2xl my-2 text-center">Vue Calendar</h1>
+    <section class="mx-2 flex justify-between">
+      <h2 class="font-bold">{{ currentMonthName }}</h2>
+      <h2 class="font-bold">{{ currentYear }}</h2>
+    </section>
     <section class="flex my-2">
       <p
         class="text-center"
@@ -12,7 +16,18 @@
       </p>
     </section>
     <section class="flex flex-wrap">
-      <p class="text-center" style="width: 14.28%" v-for="num in 30" :key="num">
+      <p
+        class="text-center"
+        style="width: 14.28%"
+        v-for="num in startDay()"
+        :key="num"
+      ></p>
+      <p
+        class="text-center"
+        style="width: 14.28%"
+        v-for="num in daysInMonth(currentYear, currentMonth)"
+        :key="num"
+      >
         {{ num }}
       </p>
     </section>
@@ -23,8 +38,19 @@
 export default {
   data() {
     return {
+      currentMonth: new Date().getMonth() + 1,
+      currentMonthName: new Date().toLocaleString("default", { month: "long" }),
+      currentYear: new Date().getFullYear(),
       days: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
     };
+  },
+  methods: {
+    daysInMonth(year, month) {
+      return new Date(year, month, 0).getDate();
+    },
+    startDay() {
+      return new Date(this.currentYear, this.currentMonth - 1, 1).getDay();
+    },
   },
 };
 </script>
