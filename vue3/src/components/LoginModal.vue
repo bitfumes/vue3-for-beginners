8afc5a82574669ba01598c18542d83cb8e62f8e8
sarthaksavvy <bitfumes@gmail.com>
2020-10-04 15:20:20 +0530

    33. Vue3 - Create Login Form

--- vue3/src/components/LoginModal.vue
+++ vue3/src/components/LoginModal.vue
@@ -5,15 +5,58 @@
   ></section>
   <div class="absolute inset-0">
     <div class="flex h-full">
-      <div class="z-30 m-auto bg-white p-2 rounded shadow">
-        <h1>Login</h1>
+      <div class="z-30 m-auto bg-white p-2 rounded shadow w-1/3">
+        <div class="p-2 border">
+          <h1 class="text-2xl text-center">Login</h1>
+          <form class="p-2 my-2" @submit.prevent="submit">
+            <div class="my-4">
+              <label>Email or Username</label>
+              <input
+                v-model="form.email"
+                class="rounded shadow p-2 w-full"
+                placeholder="Enter your email or username"
+              />
+            </div>
+            <div class="my-4">
+              <label>Password</label>
+              <input
+                v-model="form.password"
+                class="rounded shadow p-2 w-full"
+                type="password"
+                placeholder="enter your password"
+              />
+            </div>
+            <div class="my-4">
+              <button
+                type="submit"
+                class="w-full rounded shadow-md bg-gradient-to-r from-red-800 to-pink-800 text-white p-2"
+              >
+                Login
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      form: {
+        email: "",
+        password: "",
+      },
+    };
+  },
+  methods: {
+    submit() {
+      // submit the form
+    },
+  },
+};
 </script>
 
 <style></style>
