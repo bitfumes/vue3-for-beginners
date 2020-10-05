3550f9f38cf949b488daebaf82f8a84e1fb58702
sarthaksavvy <bitfumes@gmail.com>
2020-10-06 01:15:16 +0530

    55. Vue3 - userCrud pagination

--- vue3/src/pages/UserCrud.vue
+++ vue3/src/pages/UserCrud.vue
@@ -29,6 +29,28 @@
             </tr>
           </tbody>
         </table>
+        <div class="flex justify-between">
+          <button
+            class="px-3 py-2 border rounded"
+            @click="prev"
+            :disabled="state.users.page === 1"
+            :class="state.users.page === 1 ? 'bg-gray-100' : 'hover:shadow'"
+          >
+            Prev
+          </button>
+          <button
+            class="px-3 py-2 border rounded"
+            @click="next"
+            :disabled="state.users.page === state.users.total_pages"
+            :class="
+              state.users.page === state.users.total_pages
+                ? 'bg-gray-100'
+                : 'hover:shadow'
+            "
+          >
+            Next
+          </button>
+        </div>
       </div>
     </div>
   </section>
@@ -40,14 +62,25 @@ import axios from "axios";
 export default {
   setup() {
     const state = reactive({
-      users: [],
+      users: {},
     });
+
     onMounted(async () => {
       const { data } = await axios.get("https://reqres.in/api/users");
       state.users = data;
     });
 
-    return { state };
+    async function next() {
+      const { data } = await axios.get("https://reqres.in/api/users?page=2");
+      state.users = data;
+    }
+
+    async function prev() {
+      const { data } = await axios.get("https://reqres.in/api/users?page=1");
+      state.users = data;
+    }
+
+    return { state, next, prev };
   },
 };
 </script>
